a1914b655bf54111da3ae4a1753ba74150510830
Alina369 <lav0903@mail.ru>
2024-02-05 16:57:08 +0300

    all done

--- homework.py
+++ homework.py
@@ -65,10 +65,20 @@ def test_find_suitable_user():
 
     # TODO найдите пользователя с именем "Olga"
     suitable_users = None
+    for user in users:
+        if user['name'] == 'Olga':
+            suitable_users = user
+            print(suitable_users)
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
+
+    for user in users:
+        if user['age'] < 20:
+            suitable_users.append(user)
+            print(suitable_users)
+
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -92,16 +102,23 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def readable_function(func, **kwargs):
+    func_name = func.__name__.replace('_', ' ').title()
+    func_args = ', '.join([f"{value}" for value in kwargs.values()])
+    actual_result = f"{func_name} [{func_args}]"
+    return actual_result
+
+
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = readable_function(open_browser, browser_name=browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = readable_function(go_to_companyname_homepage, page_url=page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = readable_function(find_registration_button_on_login_page, page_url=page_url, button_text=button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
